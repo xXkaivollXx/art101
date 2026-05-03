0fd69af15a086e194de832970bf6cbfca9923d94
kai-voll <kaivoll@gmail.com>
2026-05-02 21:18:22 -0700

    Create env03.js

---
+++ Environ/env03.js
@@ -0,0 +1,11 @@
+let environmentTitle = "The Lands Between";
+
+let environmentElements = ["space", "ruins", "rubble", "enemies"];
+
+let mainEntity = {
+    name: "The Old World",
+    type: "soft border",
+    mood: "suspicious",
+    isMoving: true,
+    favoriteElement: environmentElements[1]
+};
