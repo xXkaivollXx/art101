b79a810530458162e8c48e315e0d942bb4ed9743
kai-voll <kaivoll@gmail.com>
2026-05-11 14:07:45 -0700

    Update env03.js

--- Environ/env03.js
+++ Environ/env03.js
@@ -9,3 +9,31 @@ let mainEntity = {
     isMoving: true,
     favoriteElement: environmentElements[1]
 };
+
+$(document).ready(function () {
+
+    // Show the images
+    $("#choose-calling").click(function () {
+        $(".topzone").fadeIn(1000);
+    });
+
+    // Clickable class choices
+    $("#prophet").click(function () {
+        $("#environment-output").html("<h2>You chose Prophet!</h2>");
+    });
+
+    $("#prisoner").click(function () {
+        $("#environment-output").html("<h2>You chose Prisoner!</h2>");
+    });
+
+    $("#vagabond").click(function () {
+        $("#environment-output").html("<h2>You chose Vagabond!</h2>");
+    });
+
+});
+
+$("#choose-calling").click(function () {
+
+    $(".topzone").css("display", "flex").hide().fadeIn(1000);
+
+});
