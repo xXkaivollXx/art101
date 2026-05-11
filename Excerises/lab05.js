40e0085fc0d6c2c570124992cb296c9f0089b271
kai-voll <kaivoll@gmail.com>
2026-05-11 15:30:06 -0700

    Update lab05.js

--- Excerises/lab05.js
+++ Excerises/lab05.js
@@ -1,84 +1,14 @@
-friends = ["Pim", "Jim", "Slim", "Whim"]// track counts
-let hangoutCount = {
-    Pim: 0,
-    Jim: 0,
-    Slim: 0,
-    Whim: 0
-};
+function askNumber(whatNumber) {
+    let userNumber = prompt("Guess 1-10?");
 
-// show image
-function showFriendImage(friendName) {
-    // hide all images first
-    $("img").hide();
-    // show only the selected one
-    $("body").append("<img class='img' src='" + friendName + "laugh.jpeg'>");
-}
-
-
-// hide all images
-$("img").hide();
-
-
-function FriendClick(friendName) {
-    // increase count
-    hangoutCount[friendName]++;
-
-    console.log(friendName + " clicked");
-    console.log("Hangouts with " + friendName + ": " + hangoutCount[friendName]);
-
-    // check if hit 5 hangouts
-    if (hangoutCount[friendName] === 5) {
-        let otherFriends = friends.filter(f => f !== friendName);
-
-        let suggestion = otherFriends[0];
-        let minCount = hangoutCount[suggestion];
-
-        for (let i = 1; i < otherFriends.length; i++) {
-            let f = otherFriends[i];
-            if (hangoutCount[f] < minCount) {
-                minCount = hangoutCount[f];
-                suggestion = f;
-            }
-        }
-
-        $("#suggestion-box").html(
-            "<p>You've hung out with " + friendName + " 5 times. Try hanging out with <b>"
-            + suggestion + "</b> (" + minCount + " hangouts).</p>"
-        );
+    if (userNumber == whatNumber) {
+        $("#output").html("That's Right!!");
     }
-
-    // show image
-    showFriendImage(friendName);
-
-    // update count on page
-    $("#count-" + friendName).remove();
-
-    $("#" + friendName).append(
-        "<p id='count-" + friendName + "'>Hangouts: " + hangoutCount[friendName] + "</p>"
-    );
+    else {
+        $("#output").html("Try Again :(");
+    }
 }
 
-// friend clicks
-$("#Pim").click(function () {
-    console.log("Pim clicked");
-    showFriendImage("Pim");
-    FriendClick("Pim");
+$("#good-button").click(function () {
+    askNumber(3);
 });
-
-$("#Jim").click(function () {
-    console.log("Jim clicked");
-    showFriendImage("Jim");
-    FriendClick("Jim");
-});
-
-$("#Slim").click(function () {
-    console.log("Slim clicked");
-    showFriendImage("Slim");
-    FriendClick("Slim");
-});
-
-$("#Whim").click(function () {
-    console.log("Whim clicked");
-    showFriendImage("Whim");
-    FriendClick("Whim");
-}); 
